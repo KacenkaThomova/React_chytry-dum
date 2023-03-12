447155f5d172a57dfbb7b89105bbb0b106cb1f10
Katerina Thomova <113352240+KacenkaThomova@users.noreply.github.com>
2023-03-12 19:14:48 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
+import { Header } from './components/Header/Header';
+import { Dashboard } from './components/Dashboard/Dashboard';
+import smartHomeData from './smartHomeData';
 
-const App = () => (
-  <>
 
-  </>
-);
+const App = () => {
+  return(
+    <div className="container">
+      <Header title="Smart House"/>
+      <Dashboard data={smartHomeData}/>
+    </div>
+)};
 
 createRoot(
   document.querySelector('#app'),
