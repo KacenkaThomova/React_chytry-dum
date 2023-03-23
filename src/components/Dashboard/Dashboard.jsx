3e73de6099d89ea3201fc5a650be2ad2bd996ad0
Katerina Thomova <113352240+KacenkaThomova@users.noreply.github.com>
2023-03-23 08:28:48 +0100

    Update Dashboard.jsx

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -9,17 +9,13 @@ export const Dashboard = ({data}) => {
     return (
     <main className="dashboard">
         <Lights
-         key={data.lights}
          lights={data.lights}/>
         <Climate
-         key={data.temperature}
          temperature={data.climate.temperature}
          humidity={data.climate.humidity}/>
         <Blinds
-         key={data.blinds}
          state={data.blinds}/>
         <Energy
-        key={data.energyConsumption.electricity}
         electricity={data.energyConsumption.electricity}
         water={data.energyConsumption.water}/>
         
